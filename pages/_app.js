d6029b5893e6ecb97b011338e2c8c29d3e7b7660
Priyanshu <vishwakarmapriyanshu98@gmail.com>
2025-01-05 19:21:02 +0530

    updated index.js

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "../styles/global.css"; // Global styles
+import { useState } from "react";
+import axios from "axios";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import "../styles/global.css";
+// import "../styles/global.css";
 
 export default function Home() {
   return (
